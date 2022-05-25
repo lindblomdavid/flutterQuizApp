fe2dc16cba07f5b0f02a450c82c79ff176a38ff5
FlutLab.io <>
2022-05-25 06:50:57 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final _questions = const [
+  final _questions = [
     {
       'questionText': 'Bajs?',
       'answers': [
